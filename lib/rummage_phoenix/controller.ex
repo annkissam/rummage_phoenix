d547fb830ad468260f3faf987d7d64a795eb21a3
Adi Iyengar <aditya7iyengar@gmail.com>
2017-03-05 10:58:42 -0500

    Split responsibilities:
    - Each hook now has its own controller helpers

--- lib/rummage_phoenix/controller.ex
+++ lib/rummage_phoenix/controller.ex
@@ -9,7 +9,7 @@ defmodule Rummage.Phoenix.Controller do
   ```elixir
   defmodule MyApp.ProductController do
     use MyApp.Web, :controller
-    use Rummage.Phoenix.Controller, struct: :product, helper: MyApp.Router.Helpers
+    use Rummage.Phoenix.Controller, struct: :product, helpers: MyApp.Router.Helpers
   end
   ```
 
@@ -18,7 +18,7 @@ defmodule Rummage.Phoenix.Controller do
   ```elixir
   defmodule MyApp.ProductController do
     use MyApp.Web, :controller
-    use Rummage.Phoenix.Controller, struct: :product, helper: MyApp.Router.Helpers,
+    use Rummage.Phoenix.Controller, struct: :product, helpers: MyApp.Router.Helpers,
       only: [:search, :sort]
   end
   ```
@@ -26,34 +26,14 @@ defmodule Rummage.Phoenix.Controller do
   """
 
   @doc """
-  This macro includes the helper functions from different Rummage.Phoenix.Controllers
+  This macro includes the helpers functions from different Rummage.Phoenix.Controllers
   """
   defmacro __using__(opts) do
     used_hooks = (opts[:only] || [:search, :sort, :paginate])
       |> Enum.map(&Atom.to_string &1)
 
     quote do
-      defp build_rummage_params(params) do
-        unquote(Enum.each(used_hooks, fn(hook) ->
-          quote do
-            apply(__MODULE__, unquote(String.to_atom("rummage_#{hook}_params")), [params])
-          end
-        end))
-      end
+      plug Rummage.Phoenix.Plug, %{hooks: unquote(used_hooks)}
     end
-
-    if "search" in used_hooks do
-      quote do
-        use Rummage.Phoenix.SearchController, struct: unquote(opts[:struct]), helper: unquote(opts[:helper])
-      end
-    end
-
-    # Enum.each(used_hooks, fn(hook) ->
-    #   controller = Module.concat("Rummage.Phoenix", "#{Macro.camelize(hook)}Controller")
-
-    #   quote do
-    #     use unquote(controller), struct: unquote(opts[:struct]), helper: unquote(opts[:helper])
-    #   end
-    # end)
   end
 end
