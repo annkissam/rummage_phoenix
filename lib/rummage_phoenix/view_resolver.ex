9981d635368ab1e5015c853133716bfc38fcb8c1
Adi Iyengar <aditya7iyengar@gmail.com>
2017-04-25 16:59:08 -0400

    Fix making helpers and struct from namespace

--- lib/rummage_phoenix/view_resolver.ex
+++ lib/rummage_phoenix/view_resolver.ex
@@ -1,14 +1,14 @@
 defmodule Rummage.Phoenix.ViewResolver do
-  def make_helpers_name_from_topmost_namespace do
-    "#{__MODULE__}"
+  def make_helpers_name_from_topmost_namespace(view_module) do
+    "#{view_module}"
     |> String.split(".")
     |> Enum.at(1)
     |> (& "Elixir." <> &1 <> ".Router.Helpers").()
     |> String.to_atom
   end
 
-  def make_struct_name_from_bottommost_namespace do
-    "#{__MODULE__}"
+  def make_struct_name_from_bottommost_namespace(view_module) do
+    "#{view_module}"
     |> String.split(".")
     |> Enum.at(-1)
     |> String.split("View")
