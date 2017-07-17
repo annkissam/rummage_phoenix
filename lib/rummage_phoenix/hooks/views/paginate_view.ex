225a2b15e1adde21cfba591b5c36b2e020d59f4a
Adi Iyengar <aditya7iyengar@gmail.com>
2017-07-17 18:09:01 -0400

    Clean up pagination links:
    - Clean up slug params support
    - Clean up default per page
    - Add max page links
    - Add ThemeAdapter

--- lib/rummage_phoenix/hooks/views/paginate_view.ex
+++ lib/rummage_phoenix/hooks/views/paginate_view.ex
@@ -21,11 +21,12 @@ defmodule Rummage.Phoenix.PaginateView do
   ```
 
   """
-
+  use Rummage.Phoenix.ThemeAdapter
   import Phoenix.HTML
+  alias Rummage.Phoenix.{BootstrapAdapter}
 
   @doc """
-  This macro includes the helpers functions for pagination
+  This macro includes the helper functions for pagination
 
   Provides helper functions `pagination_link/3` for creating pagination links in an html.eex
   file of using `Phoenix`.
@@ -40,113 +41,124 @@ defmodule Rummage.Phoenix.PaginateView do
   ```
   """
   def pagination_link(conn, rummage, opts \\ []) do
+    pagination_links do
+      [first_page_link(conn, rummage, opts)] ++
+      [previous_page_link(conn, rummage, opts)] ++
+      middle_page_links(conn, rummage, opts) ++
+      [next_page_link(conn, rummage, opts)] ++
+      [last_page_link(conn, rummage, opts)]
+      |> Enum.join("\n")
+    end
+  end
+
+  defp first_page_link(conn, rummage, opts) do
     paginate_params = rummage["paginate"]
 
-    per_page = paginate_params["per_page"]
-      |> (fn(x) -> is_nil(x) && Rummage.Phoenix.default_per_page || String.to_integer(x) end).()
+    page = String.to_integer(paginate_params["page"] || "1")
+    per_page = String.to_integer(paginate_params["per_page"] || "1")
+    max_page_links = String.to_integer(paginate_params["max_page_links"] || "4")
 
+    case page - 1 <= (max_page_links / 2) do
+      true -> page_link "#", :disabled, do: "First"
+      false ->
+        page_link index_path(opts, [conn, :index,
+          transform_params(rummage, per_page, 1, opts)]), do: "First"
+    end
+  end
+
+  defp previous_page_link(conn, rummage, opts) do
+    paginate_params = rummage["paginate"]
 
     page = String.to_integer(paginate_params["page"] || "1")
-    max_page = String.to_integer(paginate_params["max_page"] || "1")
+    per_page = String.to_integer(paginate_params["per_page"] || "1")
 
-    if max_page > 1 do
-      raw """
-      <nav aria-label="...">
-      <ul class="pagination">
-        #{conn |> get_raw_links(rummage, per_page, page, max_page, opts) |> Enum.join("\n")}
-      </ul>
-      </nav>
-      """
-    else
-      ""
+    case page <= 1 do
+      true -> page_link "#", :disabled, do: "Previous"
+      false ->
+        page_link index_path(opts, [conn, :index,
+          transform_params(rummage, per_page, page - 1, opts)]), do: "Previous"
     end
   end
 
-  defp get_raw_links(conn, rummage, per_page, page, max_page, opts) do
-    slugs = opts[:slugs] || nil
-    slug_params = opts[:slug_params] || nil
+  defp middle_page_links(conn, rummage, opts) do
+    paginate_params = rummage["paginate"]
 
-    raw_links = cond do
-      page <= 1 ->
-        [raw_disabled_link("Previous")]
-      slugs && slug_params ->
-        [
-          raw_link("Previous",
-            apply(opts[:helpers], String.to_atom("#{opts[:struct]}_path"),
-              get_helper_params(conn, slugs, slug_params, rummage, per_page, page - 1)))
-        ]
-      true ->
-        [
-          raw_link("Previous",
-            apply(opts[:helpers], String.to_atom("#{opts[:struct]}_path"),
-              [conn, :index, %{rummage: Map.put(rummage, "paginate", %{"per_page"=> per_page, "page"=> page - 1})}]))
-        ]
+    page = String.to_integer(paginate_params["page"] || "1")
+    per_page = String.to_integer(paginate_params["per_page"] || "1")
+    max_page = String.to_integer(paginate_params["max_page"] || "1")
+    max_page_links = opts[:max_page_links] || Rummage.Phoenix.default_max_page_links
+
+    lower_limit = cond do
+      page <= div(max_page_links, 2) -> 1
+      page >= (max_page - div(max_page_links, 2)) -> max_page - max_page_links + 1
+      true -> page - div(max_page_links, 2)
     end
 
-    raw_links = raw_links ++ Enum.map(1..max_page, fn(x) ->
-      case page == x do
+    upper_limit = lower_limit + max_page_links - 1
+
+    Enum.map(lower_limit..upper_limit, fn(page_num) ->
+      cond do
+        page == page_num -> page_link "#", :active, do: page_num
+        page_num > max_page -> ""
         true ->
-          raw_active_link(x)
-        _ ->
-          cond do
-            slugs && slug_params ->
-              raw_link(x,
-                apply(opts[:helpers], String.to_atom("#{opts[:struct]}_path"),
-                  get_helper_params(conn, slugs, slug_params, rummage, per_page, x)))
-            true ->
-              raw_link(x,
-                apply(opts[:helpers], String.to_atom("#{opts[:struct]}_path"),
-                  [conn, :index, %{rummage: Map.put(rummage, "paginate", %{"per_page"=> per_page, "page"=> x})}]))
-          end
+          page_link index_path(opts, [conn, :index,
+            transform_params(rummage, per_page, page_num, opts)]), do: page_num
       end
     end)
+  end
 
-    raw_links ++ cond do
-      page == max_page ->
-        [raw_disabled_link("Next")]
-      slugs && slug_params ->
-        [
-          raw_link("Next",
-            apply(opts[:helpers], String.to_atom("#{opts[:struct]}_path"),
-              get_helper_params(conn, slugs, slug_params, rummage, per_page, page + 1)))
-        ]
-      true ->
-        [
-          raw_link("Next",
-            apply(opts[:helpers], String.to_atom("#{opts[:struct]}_path"),
-              [conn, :index, %{rummage: Map.put(rummage, "paginate", %{"per_page"=> per_page, "page"=> page + 1})}]))
-        ]
+  defp next_page_link(conn, rummage, opts) do
+    paginate_params = rummage["paginate"]
+
+    page = String.to_integer(paginate_params["page"] || "1")
+    per_page = String.to_integer(paginate_params["per_page"] || "1")
+    max_page = String.to_integer(paginate_params["max_page"] || "1")
+
+    case page >= max_page do
+      true -> page_link "#", :disabled, do: "Next"
+      false ->
+        page_link index_path(opts, [conn, :index,
+          transform_params(rummage, per_page, page + 1, opts)]), do: "Next"
     end
   end
 
-  defp raw_disabled_link(name) do
-    """
-    <li class="page-item disabled">
-      <a class="page-link" href="#" tabindex="-1">#{name}</a>
-    </li>
-    """
+  defp last_page_link(conn, rummage, opts) do
+    paginate_params = rummage["paginate"]
+
+    page = String.to_integer(paginate_params["page"] || "1")
+    per_page = String.to_integer(paginate_params["per_page"] || "1")
+    max_page_links = String.to_integer(paginate_params["max_page_links"] || "4")
+    max_page = String.to_integer(paginate_params["max_page"] || "1")
+
+    case page >= (max_page - max_page_links / 2) do
+      true -> page_link "#", :disabled, do: "Last"
+      false ->
+        page_link index_path(opts, [conn, :index,
+          transform_params(rummage, per_page, max_page, opts)]), do: "Last"
+    end
   end
 
-  defp raw_link(name, url) do
-    """
-    <li class="page-item">
-      <a class="page-link" href="#{url}">#{name}</a>
-    </li>
-    """
+  defp transform_params(rummage, per_page, page, opts)
+  defp transform_params(rummage, per_page, page, %{slugs: slugs, slugs_params: slugs_params} = opts) do
+    rummage = %{rummage: Map.put(rummage, "paginate", %{"per_page"=> per_page, "page"=> page})}
+    slugs ++ Map.merge(rummage, slugs_params)
+  end
+  defp transform_params(rummage, per_page, page, opts) do
+    %{rummage:
+        Map.put(rummage, "paginate",
+        %{"per_page"=> per_page, "page"=> page})
+    }
   end
 
-  defp raw_active_link(name) do
-    """
-    <li class="page-item active">
-      <a class="page-link" href="#">#{name} <span class="sr-only">(current)</span></a>
-    </li>
-    """
+  defp index_path(opts, params) do
+    helpers = opts[:helpers]
+    path_function_name = String.to_atom("#{opts[:struct]}_path")
+
+    apply(helpers, path_function_name, params)
   end
 
-  defp get_helper_params(conn, slugs, slug_params, rummage, per_page, page) do
-    slugged = [conn, :index | slugs]
-    rummage_page = %{rummage: Map.put(rummage, "paginate", %{"per_page"=> per_page, "page"=> page})}
-    full_params = Map.merge(rummage_page, slug_params)
-    helper_params_splat = slugged ++ [full_params]
+  defp per_page(pagination_params) do
+    per_page = paginate_params["per_page"]
+      |> (&(is_nil(&1) && Rummage.Phoenix.default_per_page || String.to_integer(&1))).()
   end
 end
