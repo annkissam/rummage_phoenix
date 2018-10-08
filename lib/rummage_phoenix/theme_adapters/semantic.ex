21b751b2e28eed996871e2643bb1949dc9e8dbcd
Adi Iyengar <aditya7iyengar@gmail.com>
2018-10-08 09:39:25 -0400

    Wrap up Semantic Pagination view

---
+++ lib/rummage_phoenix/theme_adapters/semantic.ex
@@ -0,0 +1,59 @@
+defmodule Rummage.Phoenix.Semantic do
+  @moduledoc """
+  This is the theme adapter for Semantic.
+  """
+
+  alias Rummage.Phoenix.PaginateView, as: PView
+
+  def pagination_links(opts, do: expression) do
+    Phoenix.HTML.Tag.content_tag :div, [role: "navigation",
+      class: "ui pagination menu"], do: expression
+  end
+
+  def page_link(text, href, opts) do
+    class = Keyword.get(opts, :class, "")
+    tabindex = Keyword.get(opts, :tabindex, -1)
+
+    Phoenix.HTML.Link.link([to: href, class: "item #{class}",
+      tabindex: tabindex], do: text)
+  end
+
+  def link_class_fn(:paginate, rummage) do
+    fn(page) ->
+      case page do
+        page when page in ~w(first prev)a ->
+          PView.rummage_pagination_current?(1, rummage) && "disabled" || ""
+        page when page in ~w(next last)a ->
+          PView.rummage_pagination_current?(:max, rummage) && "disabled" || ""
+        page ->
+          PView.rummage_pagination_current?(page, rummage) && "active" || ""
+      end
+    end
+  end
+
+  def default_text_fn(:paginate) do
+    fn(page) ->
+      case page do
+        :first -> "«"
+        :prev -> "⟨"
+        :next -> "⟩"
+        :last -> "»"
+        page -> page
+      end
+    end
+  end
+
+  def ellipsis do
+    page_link("...", "#", class: "disabled")
+  end
+
+  def sort_link(url, do: html, content_tag: content_tag, class: class) do
+    Phoenix.HTML.Tag.content_tag content_tag, [class: class, href: url], do: html
+  end
+
+  def sort_link(url, opts \\ []) do
+    sort_link(url, do: Keyword.fetch!(opts, :do),
+                   content_tag: Keyword.get(opts, :content_tag, :a),
+                   class: Keyword.get(opts, :class, "page-link"))
+  end
+end
