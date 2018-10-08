3427358c36d806ea74e38d1998a5e28e98a922be
Adi Iyengar <aditya7iyengar@gmail.com>
2018-10-08 09:01:04 -0400

    Clean up dynamic functions

--- lib/rummage_phoenix/controller.ex
+++ lib/rummage_phoenix/controller.ex
@@ -9,55 +9,64 @@ defmodule Rummage.Phoenix.Controller do
 
   defmacro __using__(opts) do
     rummage_ecto_app = Keyword.get(opts, :rummage_ecto_app, nil)
+    schema = Keyword.get(opts, :rummage_schema, nil)
 
     quote do
-      import unquote(__MODULE__)
-
       @doc false
       def rummage_params(nil), do: rummage_params(%{})
       def rummage_params(%{} = rummage_params) do
-        Enum.reduce(~w{search sort paginate}, %{}, fn(key, acc) ->
-          case Map.get(rummage_params, key) do
-            nil -> __call_fn__(key, acc)
-            value ->
-              Map.put(acc,
-                      :"#{key}",
-                      apply(unquote(__MODULE__), :"__rummage_#{key}_params__", [value]))
-          end
-        end)
+        unquote(__MODULE__).rummage_params(rummage_params,
+                                           rummage_ecto_app,
+                                           unquote(schema))
       end
 
-      defp __call_fn__(key, acc) do
-        case function_exported?(__MODULE__, :"__rummage_default_#{key}_params__", 0) do
-          true ->
-            Map.put(acc,
-                    :"#{key}",
-                    apply(__MODULE__, :"__rummage_default_#{key}_params__", []))
-          false -> acc
+      defp rummage_ecto_app do
+        case unquote(rummage_ecto_app) do
+          nil -> Application.get_application(__MODULE__)
+          app -> app
         end
-      end
-
-      @doc false
-      def __rummage_default_paginate_params__ do
-        per_page = case unquote(rummage_ecto_app) do
-          nil -> Rummage.Ecto.Config.per_page
-          app -> Rummage.Ecto.Config.per_page(app)
-        end
-
-        %{per_page: per_page, page: 1}
       end
     end
   end
 
   @doc false
-  def __rummage_search_params__(params) do
+  def rummage_params(%{} = rummage_params, rummage_ecto_app, rummage_schema) do
+    Enum.reduce(~w{search sort paginate}, %{}, fn(key, acc) ->
+      case Map.get(rummage_params, key) do
+        nil ->
+          __get_default_params__(key, acc, rummage_ecto_app, rummage_schema)
+        value ->
+          Map.put(acc, :"#{key}",
+                  apply(__MODULE__,
+                                :"__format_#{key}_params__",
+                                [value]))
+      end
+    end)
+  end
+
+  @doc false
+  defp __get_default_params__(key, acc, rummage_ecto_app, schema) do
+    case function_exported?(Rummage.Phoenix.Controller,
+                            :"__get_default_#{key}_params__", 2) do
+      true ->
+        Map.put(acc,
+                :"#{key}",
+                apply(Rummage.Phoenix.Controller,
+                      :"__get_default_#{key}_params__",
+                      [rummage_ecto_app, schema]))
+      false -> acc
+    end
+  end
+
+  @doc false
+  def __format_search_params__(params) do
     params
     |> Map.to_list()
-    |> Enum.map(&__rummage_format_search_param__/1)
+    |> Enum.map(&__format_search_param__/1)
     |> Enum.into(%{})
   end
 
-  defp __rummage_format_search_param__({key, val = %{}}) do
+  defp __format_search_param__({key, val = %{}}) do
     val = val
       |> Map.to_list()
       |> Enum.map(fn {k, v} ->
@@ -68,12 +77,12 @@ defmodule Rummage.Phoenix.Controller do
     {:"#{key}", val}
   end
 
-  defp __rummage_format_search_param__({key, val}) do
+  defp __format_search_param__({key, val}) do
     {:"#{key}", val}
   end
 
   @doc false
-  def __rummage_sort_params__(params) do
+  def __format_sort_params__(params) do
     params
     |> Map.to_list()
     |> Enum.map(fn {k, v} -> {:"#{k}", :"#{v}"} end)
@@ -81,12 +90,28 @@ defmodule Rummage.Phoenix.Controller do
   end
 
   @doc false
-  def __rummage_paginate_params__(params) do
+  def __format_paginate_params__(params) do
     Enum.reduce(~w{per_page page}, %{}, fn(key, acc) ->
       case Map.get(params, key) do
         nil -> acc
         value -> Map.put(acc, :"#{key}", String.to_integer(value))
       end
     end)
+  end
+
+  @doc false
+  def __get_default_paginate_params__(rummage_ecto_app, rummage_schema) do
+    per_page =
+      with nil <- rummage_schema,
+           app when not is_nil(app) <- rummage_ecto_app
+      do
+        Rummage.Ecto.Config.per_page(app)
+      else
+        schema when not is_nil(schema) -> schema.__rummage_defaults__[:per_page]
+        nil -> Rummage.Ecto.Config.per_page
+      end
+
+
+    %{per_page: per_page, page: 1}
   end
 end
